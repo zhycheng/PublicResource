fce1ac9b26e355d1278239d9a3a33a0f823d0dff
张译成 <664627300@qq.com>
2020-04-10 19:07:57 +0800

    新建一个文件
    新建一个文件

---
+++ severs_debug.php
@@ -0,0 +1,48 @@
+{
+	"version": 1,
+	"severVerson": "1.100.0",
+	"updateAddress": "http://192.168.1.7/NuclearGame/trunk/update.php",
+	"rootAddress": "http://192.168.1.7/NuclearGame/trunk/",
+	"defaultSeverID": 10012,
+	"announcement": "http://192.168.1.7/dragonball/notice_ios.php",
+	"serverInUpdate": "当前网络与服务器通信不畅，请切换到2G/3G网络重试，QQ群:",
+	"serverInGray": "当前是灰度服务器",
+	"updateByteSize": 8824735,
+	"updateMsg": "修改大转盘文案显示和决斗文案",
+	"inStoreUpdate": "http://1251001040.imgcache.qzoneapp.com/1251001040/download/91/index_ios.html",
+	"internalAnnouncementFilePath": "http://192.168.1.7/NuclearGame/anncounmentConfig/announcementconfig.json",
+	"internalAnnouncementVersion": 1,
+	"severs": [{
+			"id": 10011,
+			"name": "本地服",
+			"address": "127.0.0.1",
+			"port": 25524,
+			"state": "general",
+			"order": 1
+		},
+		{
+			"id": 10012,
+			"name": "内网服",
+			"address": "192.168.1.7",
+			"port": 25524,
+			"state": "general",
+			"order": 2
+		},
+		{
+			"id": 10013,
+			"name": "线上服",
+			"address": "107.150.99.72",
+			"port": 25524,
+			"state": "general",
+			"order": 3
+		},
+		{
+			"id": 10014,
+			"name": "内网服2",
+			"address": "221.232.56.250",
+			"port": 25524,
+			"state": "general",
+			"order": 4
+		}
+	]
+}
